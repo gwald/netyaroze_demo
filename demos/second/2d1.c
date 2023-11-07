2f60f1a9ba78745877684b99bea97a8f3bc7358d
gwald <1996mgf@gmail.com>
2023-11-07 22:13:02 +1100

    Add files via upload

---
+++ demos/second/2d1.c
@@ -0,0 +1,195 @@
+// first and simplest 2d module
+
+
+
+#include "2d1.h"
+
+
+	// NO overall initialiser
+
+
+
+
+void InitialiseTexture (long address)
+{
+	RECT rect;
+	GsIMAGE timInfo;
+
+	GsGetTimInfo((u_long *)(address+4), &timInfo);
+
+	rect.x = timInfo.px;	
+	rect.y = timInfo.py;		
+	rect.w = timInfo.pw;	
+	rect.h = timInfo.ph;	
+
+
+	LoadImage(&rect, timInfo.pixel);
+
+	if ( (timInfo.pmode>>3) & 0x01 ) 
+		{
+		rect.x = timInfo.cx;	
+		rect.y = timInfo.cy;	
+		rect.w = timInfo.cw;	
+		rect.h = timInfo.ch;	
+
+		LoadImage(&rect, timInfo.clut);
+   		}
+
+	DrawSync(0);
+}
+
+
+
+
+
+	// the second argument is a global declared next to the #define for
+	// the TIM file main memory address;
+	// it allows endless sprites to be easily linked to it later on
+void ProperInitialiseTexture (long address, GsIMAGE* imageInfo)
+{
+	RECT rect;
+
+		// +4 because we need to skip head of TIM file
+	GsGetTimInfo( (u_long *)(address+4), imageInfo);
+
+	rect.x = imageInfo->px;	
+	rect.y = imageInfo->py;		
+	rect.w = imageInfo->pw;	
+	rect.h = imageInfo->ph;	
+
+	LoadImage(	&rect, imageInfo->pixel);
+
+		// if image needs a CLUT, load it up 
+		// (pmode: 8 or 9 ==> texture is 4-bit or 8-bit)
+	if ( (imageInfo->pmode>>3)&0x01) 
+		{
+		rect.x = imageInfo->cx;	
+		rect.y = imageInfo->cy;	
+		rect.w = imageInfo->cw;	
+		rect.h = imageInfo->ch;	
+
+		LoadImage( &rect, imageInfo->clut);
+		}
+
+		// wait for loading to finish
+	DrawSync(0);
+}
+
+
+
+
+
+void LinkSpriteToImageInfo (GsSPRITE* sprite, GsIMAGE* imageInfo)
+{
+	int widthCompression;	  // on frame buffer
+	int tPageType;
+	int texturePageX, texturePageY, xOffset, yOffset;
+
+	InitGsSprite(sprite);
+		
+	FindTopLeftOfTexturePage(imageInfo, 
+		&texturePageX, &texturePageY, &xOffset, &yOffset);
+
+	switch(imageInfo->pmode)
+		{
+		case 0:		   // 16-bit
+			sprite->attribute |= SIXTEEN_BIT_MASK;		
+			widthCompression = 1;
+			tPageType = 2;	
+			break;
+		case 8:		   	 // 4-bit
+			widthCompression = 4;
+			tPageType = 0;
+			sprite->cx = imageInfo->cx;
+			sprite->cy = imageInfo->cy;	
+			break;
+		case 9:		   // 8-bit
+			sprite->attribute |= EIGHT_BIT_MASK;		
+			widthCompression = 2;
+			tPageType = 1;
+			sprite->cx = imageInfo->cx;
+			sprite->cy = imageInfo->cy;		
+			break;
+		default:
+			printf("Only 4, 8 and 16 bit modes supported\n");
+			assert(FALSE);		// other modes not supported
+		}
+
+	sprite->tpage 
+			= GetTPage(tPageType, 0, texturePageX, texturePageY);
+
+	sprite->w = imageInfo->pw * widthCompression;
+	sprite->h = imageInfo->ph;
+
+	sprite->u = xOffset;
+	sprite->v = yOffset;
+}
+
+
+
+
+void InitGsSprite (GsSPRITE* sprite)
+{
+		// initialise sprite to dummy
+	sprite->attribute = 0;
+	sprite->x = 0;
+	sprite->y = 0;
+	sprite->w = 0;
+	sprite->h = 0;
+	sprite->tpage = 0;
+	sprite->u = 0;
+	sprite->v = 0;
+	sprite->cx = 0;
+	sprite->cy = 0;
+	sprite->r = 128;
+	sprite->g = 128;
+	sprite->b = 128;
+	sprite->mx = 0;
+	sprite->my = 0;
+	sprite->scalex = ONE;
+	sprite->scaley = ONE;
+	sprite->rotate = 0;
+}
+
+
+
+
+
+
+	// find coords wrt tpage top left points
+void FindTopLeftOfTexturePage (GsIMAGE* imageInfo, 
+									int* x, int* y, int* u, int* v)
+{
+	int testX, testY;
+
+	testX = imageInfo->px;
+	testY = imageInfo->py;
+
+	*u = 0;
+	*v = 0;
+	
+	for (;;)
+		{
+		if (testX % 320 != 0)
+			{
+			testX--;
+			(*u)++;
+			}
+		else
+			break;
+		}
+
+	for (;;)
+		{
+		if (testY % 256 != 0)
+			{
+			testY--;
+			(*v)++;
+			}
+		else
+			break;
+		}
+
+	*x = testX;
+	*y = testY;
+}
---
+++ demos/second/object.h
@@ -0,0 +1,151 @@
+/************************************************************
+ *															*
+ *						object.h							*
+ *			   											    *															*
+ *				LPGE     29/10/96							*		
+ *															*
+ *	Copyright (C) 1996 Sony Computer Entertainment Inc.		*
+ *	All Rights Reserved										*
+ *															*
+ ***********************************************************/
+
+
+#ifndef OBJECT_H_INCLUDED
+
+#include <libps.h>
+
+#include "asssert.h"
+
+#include "tmd.h"
+
+#include "2d1.h"
+
+#include "general.h"
+	
+
+
+	// display flag: by GsSPRITE or by GsDOBJ2
+#define TMD 0
+#define SPRITE 1
+
+
+#define MAX_OBJECTS 1024
+
+
+
+typedef struct
+{
+	int id;
+	int alive;		 // boolean
+	int type;		 
+	int subType;
+	int which;	
+	
+	int displayFlag;		// TMD or SPRITE
+
+	GsIMAGE* imageInfo;
+	GsSPRITE sprite;
+	int scaleX, scaleY;
+		
+	GsDOBJ2	handler;
+	u_long modelAddress;
+	int whichModel;
+
+	int scalingFlag;
+	VECTOR scalingVector;			// scaling in 3D: volume control
+
+	VECTOR position;		// position
+	VECTOR velocity;		
+	int movementMomentumFlag;
+	int movementSpeed;
+
+	SVECTOR rotate;			// angle
+	VECTOR twist;			// change of angle
+	int rotationMomentumFlag;
+	int rotationSpeed;
+
+	GsCOORDINATE2 coord;	 // coordinate system
+	int superCoordinateObjectID;
+	MATRIX matrix;			// relation to SUPER
+
+	int inOrbit;		// boolean
+	int centralBodyID;		// id of body around which object orbits
+	int angle;		   // within circle
+	int angleIncrement;	  // speed of orbit: amount of increment per frame
+	int whichWay;		  // CLOCKWISE or ANTICLOCKWISE
+	int radius;			  // distance from central body
+	VECTOR firstVector;		  // these two lie on the plane of orbit
+	VECTOR secondVector;	  // second is perpendicular to first
+
+	int lifeTime;
+	int specialTimeLimit;
+	int secondPeriod;
+	int thirdPeriod;
+	
+	int initialHealth;
+	int currentHealth;
+	
+	int allegiance;	
+	
+	int canFire;
+	int firingRate;
+	int framesSinceLastFire;
+	int shotSpeed;
+	int shotType;
+	
+	int actionState;
+	
+	int objectIdList[12];	
+} ObjectHandler;
+
+
+
+extern ObjectHandler* ObjectArray[MAX_OBJECTS];
+
+
+
+
+		// prototypes for object.c
+
+
+
+/***************	first section: class functions				*************************/
+
+void InitialiseObjectClass (void);
+
+int FindNextLowestObjectID (int objectID);
+int FindNextHighestObjectID (int objectID);
+
+int FindNextUnusedObjectID (void);
+
+int CountNumberOfLivingObjects (void);
+int CountNumberOfLivingTypedObjects (int type);
+
+void LinkAllObjectsToModelsOrSprites (void);
+void LinkAllObjectsToTheirCoordinateSystems (void);
+
+/***************	second section: individual object functions			***********************/
+		
+void BringObjectToLife (ObjectHandler* object, int type,
+		u_long modelAddress, int whichModel, 
+			int superCoordinateObjectID);
+void KillAnObject (ObjectHandler* object);
+
+void RegisterObjectIntoObjectArray (ObjectHandler* object);
+void RemoveObjectFromObjectArray (ObjectHandler* object);
+
+void InitSingleObject (ObjectHandler* object);
+void ReviveObject (ObjectHandler* object);
+
+void SetObjectScaling (ObjectHandler* object, int scaleX,
+						int scaleY, int scaleZ);
+void SortObjectSize (ObjectHandler* object);
+
+
+	   
+
+
+
+#define OBJECT_H_INCLUDED 1
+
+#endif
